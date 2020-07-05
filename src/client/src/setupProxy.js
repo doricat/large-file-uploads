dfeec3668e7a75538006b58e5111b8e5443e23bc
doricat <doricat@outlook.com>
2020-07-06 01:16:09 +0800

    创建客户端程序

---
+++ src/client/src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = app => {
+    app.use(createProxyMiddleware("/api", {
+        target: "http://localhost:5000",
+        ws: true,
+        pathRewrite: {
+            "^/api/": "/"
+        }
+    }));
+}
